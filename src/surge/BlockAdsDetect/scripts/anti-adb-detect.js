df97725db19c3f98032ef3c11cb41524dc5254e9
kien8995 <null>
2024-04-03 17:05:58 +0000

    ✅ synced local 'src/' with remote 'dist/'

---
+++ src/surge/BlockAdsDetect/scripts/anti-adb-detect.js
@@ -0,0 +1,1 @@
+(()=>{(()=>{"use strict";var r={};const o=/(Adblock|blocking|deblocker|pagead)/g;let{body:e}=$response;const s=/(<|%3C)script[\s\S]*?(>|%3E)[\s\S]*?(<|%3C)(\/|%2F)script[\s\S]*?(>|%3E)/g;let t=(e==null?void 0:e.match(s))||[];t=(t==null?void 0:t.filter(c=>c.match(o)))||[];for(const c in t)e=e.replace(t[c],"");$done({body:e})})();})();
---
+++ src/surge/Panels/scripts/ip_check.js
@@ -0,0 +1,3 @@
+(()=>{(()=>{"use strict";var l={};$httpClient.get("http://ip-api.com/json",(n,e,o)=>{const t=JSON.parse(o),{country:c}=t,r=s(t.countryCode),{city:i}=t,{isp:a}=t,p={title:"IP Check",content:`IP: ${t.query}
+Operator: ${a}
+Location: ${i} - ${c}${r}`,icon:"globe.asia.australia.fill"};$done(p)});function s(n){const e=n.toUpperCase().split("").map(o=>127397+o.charCodeAt());return String.fromCodePoint(...e)}})();})();
