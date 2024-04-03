50883c3b42eca6163e2252fa062fcdd69cba442e
kien8995 <null>
2024-04-03 07:44:12 +0000

    ✅ synced local 'src/' with remote 'dist/'

--- src/sub-store/underlying-proxy.js
+++ src/sub-store/underlying-proxy.js
@@ -1,1 +1,1 @@
-(()=>{(()=>{var o={};function e(r,u){const{isSurge:n}=$substore.env;return n?r.map(t=>(t["underlying-proxy"]="\u26D3\uFE0F Proxy Chain",t)):r}window.operator=e})();})();
+(()=>{function n(r,o){const{isSurge:e}=$substore.env;return e?r.map(t=>(t["underlying-proxy"]="\u26D3\uFE0F Proxy Chain",t)):r}window.operator=n;})();
