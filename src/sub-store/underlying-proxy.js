e0b39710da26129014c0a3cdd83681cd4c4fb506
kien8995 <null>
2024-05-08 20:19:13 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/underlying-proxy.js
+++ src/sub-store/underlying-proxy.js
@@ -1,6 +1,5 @@
-
- /*
- fullhash:e1370f1030e67aed30a4, chunkhash:ddd18ce234be38812ffa, file:sub-store/underlying-proxy.js
+/*
+ fullhash:291cfc6c731f41cb8081, chunkhash:ddd18ce234be38812ffa, file:sub-store/underlying-proxy.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var f={183:(e,r,t)=>{var o;typeof window!="undefined"?o=window:typeof t.g!="undefined"?o=t.g:typeof self!="undefined"?o=self:o={},e.exports=o}},_={};function n(e){var r=_[e];if(r!==void 0)return r.exports;var t=_[e]={exports:{}};return f[e](t,t.exports,n),t.exports}n.n=e=>{var r=e&&e.__esModule?()=>e.default:()=>e;return n.d(r,{a:r}),r},n.d=(e,r)=>{for(var t in r)n.o(r,t)&&!n.o(e,t)&&Object.defineProperty(e,t,{enumerable:!0,get:r[t]})},n.g=function(){if(typeof globalThis=="object")return globalThis;try{return this||new Function("return this")()}catch(e){if(typeof window=="object")return window}}(),n.o=(e,r)=>Object.prototype.hasOwnProperty.call(e,r);var s={};(()=>{var e=n(183),r=n.n(e);function t(o){const{isSurge:i}=$substore.env;let u=!0;return i&&(u=!0),u?o.map(a=>(a["underlying-proxy"]="\u26D3\uFE0F Proxy Chain",a)):o}r().operator=t})()})();})();
