4d56f255a855d01915fdca6dcbc2fa4f927f5d24
kien8995 <null>
2024-05-09 20:56:29 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/Hyperweb/scripts/hyperweb.js
+++ src/surge/Hyperweb/scripts/hyperweb.js
@@ -1,5 +1,5 @@
 /*
- fullhash:3013d3ecee3e87cf1384, chunkhash:7945ef7ab48597a54fd1, file:surge/Hyperweb/scripts/hyperweb.js
+ fullhash:c76cad1b1b0a2508b624, chunkhash:7945ef7ab48597a54fd1, file:surge/Hyperweb/scripts/hyperweb.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var t={};const e={autoRenewStatus:"0",isActive:!0,expireDateMs:4072897555e3,expirationIntent:"USER_CANCELLED",productId:"ai.laso.ios.HyperWeb.yearly.subscription"};$done({body:JSON.stringify(e)})})();})();
