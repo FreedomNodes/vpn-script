ae03b4e58c3562fb0ed42300c75e764bf94c548e
kien8995 <null>
2024-05-09 20:15:19 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/AdGuard/scripts/adguard-premium.js
+++ src/surge/AdGuard/scripts/adguard-premium.js
@@ -1,5 +1,5 @@
 /*
- fullhash:164ad9a81da617ed8b41, chunkhash:9bffc8c6e6823be1195a, file:surge/AdGuard/scripts/adguard-premium.js
+ fullhash:34830ced8dbf84302b85, chunkhash:9bffc8c6e6823be1195a, file:surge/AdGuard/scripts/adguard-premium.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var r={};const t={products:[{product_id:"com.adguard.lifetimePurchase",premium_status:"ACTIVE"}]};$done({body:JSON.stringify(t)})})();})();
--- src/surge/NYMF/scripts/nymf.js
+++ src/surge/NYMF/scripts/nymf.js
@@ -1,5 +1,5 @@
 /*
- fullhash:164ad9a81da617ed8b41, chunkhash:19851b7329365e043403, file:surge/NYMF/scripts/nymf.js
+ fullhash:34830ced8dbf84302b85, chunkhash:19851b7329365e043403, file:surge/NYMF/scripts/nymf.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var r={};const e={valid:!0,state:!0,os:"ios",expires_date:1902813955,debug:["[trace][firebase] Firebase ID received from cache."]};$done({body:JSON.stringify(e)})})();})();
