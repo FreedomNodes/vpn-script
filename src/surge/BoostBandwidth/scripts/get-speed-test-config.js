96d602961c7bf65ff9f63a353f2289233455872c
kien8995 <null>
2024-05-09 21:15:35 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/BoostBandwidth/scripts/get-speed-test-config.js
+++ src/surge/BoostBandwidth/scripts/get-speed-test-config.js
@@ -1,5 +1,5 @@
 /*
- fullhash:f475625b8b5986c47489, chunkhash:a49cc7f67a1ce9136fae, file:surge/BoostBandwidth/scripts/get-speed-test-config.js
+ fullhash:541f9c1f8095cad0f64d, chunkhash:a49cc7f67a1ce9136fae, file:surge/BoostBandwidth/scripts/get-speed-test-config.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var t={};$done($httpClient.get("https://www.speedtest.net/api/ios-config.php"))})();})();
--- src/surge/NYMF/scripts/nymf.js
+++ src/surge/NYMF/scripts/nymf.js
@@ -1,5 +1,5 @@
 /*
- fullhash:f475625b8b5986c47489, chunkhash:19851b7329365e043403, file:surge/NYMF/scripts/nymf.js
+ fullhash:541f9c1f8095cad0f64d, chunkhash:19851b7329365e043403, file:surge/NYMF/scripts/nymf.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var r={};const e={valid:!0,state:!0,os:"ios",expires_date:1902813955,debug:["[trace][firebase] Firebase ID received from cache."]};$done({body:JSON.stringify(e)})})();})();
