eec3d8524c97643a8e33e68924a38afd48e3c258
kien8995 <null>
2024-05-10 11:55:12 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/Youtube/scripts/youtube.request.beta.js
+++ src/surge/Youtube/scripts/youtube.request.beta.js
@@ -1,6 +1,6 @@
 /*!
- substore-tools - version: 1.1.0
- Build fullhash:2cbf25194291235b58d5, chunkhash:4bd8373e5586a4c4698a (2024-05-10 16:24:02)
+ substore-tools - version: 1.2.0
+ Build fullhash:2cbf25194291235b58d5, chunkhash:4bd8373e5586a4c4698a (2024-05-10 18:55:09)
 
  Author: Kien Tran
 */
