82939e1910d5e45731b7e654c6e6c3539d485341
kien8995 <null>
2024-05-10 06:33:09 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/AdGuard/scripts/adguard-premium.js
+++ src/surge/AdGuard/scripts/adguard-premium.js
@@ -1,5 +1,5 @@
 /*
- fullhash:8078244a1c44f8b1932e, chunkhash:9bffc8c6e6823be1195a, file:surge/AdGuard/scripts/adguard-premium.js
+ fullhash:6f33446f5136ee72b221, chunkhash:9bffc8c6e6823be1195a, file:surge/AdGuard/scripts/adguard-premium.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var r={};const t={products:[{product_id:"com.adguard.lifetimePurchase",premium_status:"ACTIVE"}]};$done({body:JSON.stringify(t)})})();})();
