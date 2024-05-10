ae713aebc70e2f28d28df12681c18bc0dc9354c4
kien8995 <null>
2024-05-10 11:55:12 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/proxy-sort-operator.js
+++ src/sub-store/proxy-sort-operator.js
@@ -1,6 +1,6 @@
 /*!
- substore-tools - version: 1.1.0
- Build fullhash:2cbf25194291235b58d5, chunkhash:4886b771bf17d7eda1f1 (2024-05-10 16:24:02)
+ substore-tools - version: 1.2.0
+ Build fullhash:2cbf25194291235b58d5, chunkhash:4886b771bf17d7eda1f1 (2024-05-10 18:55:09)
 
  Author: Kien Tran
 */
