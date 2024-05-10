b530b50d74f3eed48e82977f8158fd9b38e3ec12
kien8995 <null>
2024-05-10 04:10:55 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/BoostBandwidth/scripts/get-speed-test-config.js
+++ src/surge/BoostBandwidth/scripts/get-speed-test-config.js
@@ -1,5 +1,5 @@
 /*
- fullhash:e4f67347ec134e5f3985, chunkhash:a49cc7f67a1ce9136fae, file:surge/BoostBandwidth/scripts/get-speed-test-config.js
+ fullhash:7b683ae1166bd3674227, chunkhash:a49cc7f67a1ce9136fae, file:surge/BoostBandwidth/scripts/get-speed-test-config.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var t={};$done($httpClient.get("https://www.speedtest.net/api/ios-config.php"))})();})();
--- src/surge/Testflight/scripts/testflight.js
+++ src/surge/Testflight/scripts/testflight.js
@@ -1,5 +1,5 @@
 /*
- fullhash:e4f67347ec134e5f3985, chunkhash:41a0d3165d160b523103, file:surge/Testflight/scripts/testflight.js
+ fullhash:7b683ae1166bd3674227, chunkhash:41a0d3165d160b523103, file:surge/Testflight/scripts/testflight.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var p={};function o(s){const t=s.match(/^\/(.*?)\/([gims]*)$/);return t?new RegExp(t[1],t[2]):new RegExp(s)}let e;typeof $argument=="undefined"?console.log("requires $argument"):$script.type==="http-response"?e=$response.body:$script.type==="http-request"?e=$request.body:console.log("script type error"),e?($argument.split("&").forEach(s=>{const[t,r]=s.split("->"),n=o(t);e=e.replace(n,r)}),$done({body:e})):(console.log("Not Modify"),$done({}))})();})();
