6c3e4830af4f04c137ffce3508e41b4037070c17
kien8995 <null>
2024-05-10 05:57:57 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/BoostBandwidth/scripts/get-speed-test-config.js
+++ src/surge/BoostBandwidth/scripts/get-speed-test-config.js
@@ -1,5 +1,5 @@
 /*
- fullhash:6f33446f5136ee72b221, chunkhash:a49cc7f67a1ce9136fae, file:surge/BoostBandwidth/scripts/get-speed-test-config.js
+ fullhash:8078244a1c44f8b1932e, chunkhash:a49cc7f67a1ce9136fae, file:surge/BoostBandwidth/scripts/get-speed-test-config.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var t={};$done($httpClient.get("https://www.speedtest.net/api/ios-config.php"))})();})();
