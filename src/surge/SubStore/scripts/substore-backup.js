bc0963eba0ca6142068918a447f27bef727a0327
kien8995 <null>
2024-05-10 07:33:02 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/SubStore/scripts/substore-backup.js
+++ src/surge/SubStore/scripts/substore-backup.js
@@ -1,5 +1,5 @@
 /*
- fullhash:5121f66e8d1d33625ec1, chunkhash:543cef4eaa42a2942597, file:surge/SubStore/scripts/substore-backup.js
+ fullhash:5704905b46a4c82c6a68, chunkhash:543cef4eaa42a2942597, file:surge/SubStore/scripts/substore-backup.js
  author: Kien Tran
  */
 (()=>{(()=>{"use strict";var t={};$done($httpClient.get("https://sub.store/api/utils/backup?action=upload"))})();})();
