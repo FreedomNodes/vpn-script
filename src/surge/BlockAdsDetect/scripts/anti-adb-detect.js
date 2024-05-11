081e616d021322bba3a3a7b1bedd64abcefeef71
kien8995 <null>
2024-05-11 18:19:14 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/BlockAdsDetect/scripts/anti-adb-detect.js
+++ src/surge/BlockAdsDetect/scripts/anti-adb-detect.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.2.3
- Build fullhash:60265d0641b17d303375, chunkhash:9ac90c4b3aa5505548f0 (2024-05-11 22:43:28)
+ Build fullhash:08ddc0fa493e2555d7e0, chunkhash:9ac90c4b3aa5505548f0 (2024-05-12 01:19:11)
 
  Author: Kien Tran
 */
--- src/surge/Quizlet/scripts/quizlet.js
+++ src/surge/Quizlet/scripts/quizlet.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.2.3
- Build fullhash:60265d0641b17d303375, chunkhash:5d28928c92120aa07c8d (2024-05-11 22:43:28)
+ Build fullhash:08ddc0fa493e2555d7e0, chunkhash:5d28928c92120aa07c8d (2024-05-12 01:19:11)
 
  Author: Kien Tran
 */
