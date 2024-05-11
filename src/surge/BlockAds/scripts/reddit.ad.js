cc3a178fcfaea2afc82c740d81048f2424ced5f1
kien8995 <null>
2024-05-11 20:05:13 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/BlockAds/scripts/reddit.ad.js
+++ src/surge/BlockAds/scripts/reddit.ad.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.3.0
- Build fullhash:08ddc0fa493e2555d7e0, chunkhash:98d7d1984e67eecaaa28 (2024-05-12 01:35:55)
+ Build fullhash:ab537083d4fc611a9697, chunkhash:98d7d1984e67eecaaa28 (2024-05-12 03:05:09)
 
  Author: Kien Tran
 */
--- src/surge/Panels/scripts/traffic_statistics.js
+++ src/surge/Panels/scripts/traffic_statistics.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.3.0
- Build fullhash:08ddc0fa493e2555d7e0, chunkhash:c971a252a0c1e06f47d5 (2024-05-12 01:35:55)
+ Build fullhash:ab537083d4fc611a9697, chunkhash:c971a252a0c1e06f47d5 (2024-05-12 03:05:09)
 
  Author: Kien Tran
 */
