ba9de61e06877103cb20ca5d5aa4e85f5143f1b9
kien8995 <null>
2024-05-12 09:43:16 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/proxy-normalize-operator.js
+++ src/sub-store/proxy-normalize-operator.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.3.0
- Build fullhash:03ee405b98dac987496a, chunkhash:3b9fdff55da63b4e7fa3 (2024-05-12 16:42:41)
+ Build fullhash:b09f208bc98a854d6574, chunkhash:3b9fdff55da63b4e7fa3 (2024-05-12 16:43:14)
 
  Author: Kien Tran
 */
--- src/sub-store/resource-cache-operator.js
+++ src/sub-store/resource-cache-operator.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.3.0
- Build fullhash:03ee405b98dac987496a, chunkhash:4290024271e8f13e3c42 (2024-05-12 16:42:41)
+ Build fullhash:b09f208bc98a854d6574, chunkhash:4290024271e8f13e3c42 (2024-05-12 16:43:14)
 
  Author: Kien Tran
 */
