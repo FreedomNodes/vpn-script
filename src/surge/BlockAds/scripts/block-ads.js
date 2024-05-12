0f74971bed15aedddcb3af74d05818f28c103688
kien8995 <null>
2024-05-12 09:28:08 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/BlockAds/scripts/block-ads.js
+++ src/surge/BlockAds/scripts/block-ads.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.3.0
- Build fullhash:03ee405b98dac987496a, chunkhash:9017d99bf26865c8bffd (2024-05-12 16:25:34)
+ Build fullhash:b09f208bc98a854d6574, chunkhash:9017d99bf26865c8bffd (2024-05-12 16:28:05)
 
  Author: Kien Tran
 */
