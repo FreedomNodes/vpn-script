4bc1980392bc260c621a8f13b2c34376590d25c7
kien8995 <null>
2024-05-14 07:40:29 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/get-flag.js
+++ src/sub-store/get-flag.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.6.0
- Build fullhash:5548988d67c7d00025d2, chunkhash:c20394ff2c8fe55e55c0 (2024-05-14 13:54:32)
+ Build fullhash:dbe4b70414b59817990c, chunkhash:c20394ff2c8fe55e55c0 (2024-05-14 14:40:26)
 
  Author: Kien Tran
 */
