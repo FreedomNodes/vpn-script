eaf233ee9863e16274f9293aeb885fe309dab7ad
kien8995 <null>
2024-05-16 17:42:49 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/proxy-name-operator.js
+++ src/sub-store/proxy-name-operator.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.7.0
- Build fullhash:beda40e993efabcd6e32, chunkhash:fc4550c26de36a5bdc50 (2024-05-17 24:28:42)
+ Build fullhash:beda40e993efabcd6e32, chunkhash:fc4550c26de36a5bdc50 (2024-05-17 24:42:47)
 
  Author: Kien Tran
 */
--- src/sub-store/tls-fingerprint.js
+++ src/sub-store/tls-fingerprint.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.7.0
- Build fullhash:beda40e993efabcd6e32, chunkhash:3e7a90c42be75656fa33 (2024-05-17 24:28:42)
+ Build fullhash:beda40e993efabcd6e32, chunkhash:3e7a90c42be75656fa33 (2024-05-17 24:42:47)
 
  Author: Kien Tran
 */
