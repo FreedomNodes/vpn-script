e74b84f84d30191532ef33add689f647b262f8cd
kien8995 <null>
2024-05-16 17:28:49 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/Panels/scripts/traffic_statistics.js
+++ src/surge/Panels/scripts/traffic_statistics.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.7.0
- Build fullhash:f0a99c45155dc40fa461, chunkhash:c971a252a0c1e06f47d5 (2024-05-16 22:28:41)
+ Build fullhash:beda40e993efabcd6e32, chunkhash:c971a252a0c1e06f47d5 (2024-05-17 24:28:42)
 
  Author: Kien Tran
 */
