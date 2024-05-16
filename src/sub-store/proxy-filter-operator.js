257f8c2a9e1cd0510459b5f57b15a525a7f799f5
kien8995 <null>
2024-05-16 17:55:49 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/proxy-filter-operator.js
+++ src/sub-store/proxy-filter-operator.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.7.0
- Build fullhash:beda40e993efabcd6e32, chunkhash:64a72b6f9b8a9d7a1c68 (2024-05-17 24:42:47)
+ Build fullhash:f0a99c45155dc40fa461, chunkhash:64a72b6f9b8a9d7a1c68 (2024-05-17 24:55:45)
 
  Author: Kien Tran
 */
