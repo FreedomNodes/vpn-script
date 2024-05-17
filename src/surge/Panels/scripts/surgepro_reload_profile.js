4fa868eeb461092dbaea5ef1510fe29a59f1eeaf
kien8995 <null>
2024-05-17 17:00:06 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/Panels/scripts/surgepro_reload_profile.js
+++ src/surge/Panels/scripts/surgepro_reload_profile.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:af4aade40780724b43e5, chunkhash:cba28f411fd244f8aa18 (2024-05-17 23:53:06)
+ Build fullhash:4f62360037c1bd4aab76, chunkhash:cba28f411fd244f8aa18 (2024-05-18 24:00:03)
 
  Author: Kien Tran
 */
