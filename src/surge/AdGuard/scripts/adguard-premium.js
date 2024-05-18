bfa1be0b0641b3ccce3d44896485569b1fd7033a
kien8995 <null>
2024-05-18 12:05:00 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/AdGuard/scripts/adguard-premium.js
+++ src/surge/AdGuard/scripts/adguard-premium.js
@@ -1,6 +1,6 @@
 /*!
  Made with ❤️ in Hanoi
- Build fullhash:051812fbaf5d62790f36, chunkhash:c46c135c48983a3f45b4 (2024-05-18 18:59:33)
+ Build fullhash:0378e221eaeb7d75c4ee, chunkhash:c46c135c48983a3f45b4 (2024-05-18 19:04:54)
 
  Author: Kien Tran
 */
--- src/surge/Panels/scripts/group_panel.js
+++ src/surge/Panels/scripts/group_panel.js
@@ -1,6 +1,6 @@
 /*!
  Made with ❤️ in Hanoi
- Build fullhash:051812fbaf5d62790f36, chunkhash:606e0eb395ab87b08cd8 (2024-05-18 18:59:33)
+ Build fullhash:0378e221eaeb7d75c4ee, chunkhash:606e0eb395ab87b08cd8 (2024-05-18 19:04:54)
 
  Author: Kien Tran
 */
