1db544a1d9a8d98f889023d123b0853afb1ea46d
kien8995 <null>
2024-05-18 20:21:15 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/sni-operator.js
+++ src/sub-store/sni-operator.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:978b3a58eee77459663a, chunkhash:11019cf5080f19c7fb75 (2024-05-19 03:17:26)
+ Build fullhash:978b3a58eee77459663a, chunkhash:11019cf5080f19c7fb75 (2024-05-19 03:21:05)
 
  Author: Kien Tran
 */
