da31bf57c77fa9240e4d6d6127f01c4a4da79358
kien8995 <null>
2024-05-18 12:15:10 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/BoostBandwidth/scripts/get-speed-test-config.js
+++ src/surge/BoostBandwidth/scripts/get-speed-test-config.js
@@ -1,6 +1,6 @@
 /*!
  Made with ❤️ in Hanoi
- Build fullhash:7dd779982ddfe83cb09b, chunkhash:c5e8b9f077650e0a1ebb (2024-05-18 19:11:02)
+ Build fullhash:2af83bd1581f295b28b6, chunkhash:c5e8b9f077650e0a1ebb (2024-05-18 19:15:03)
 
  Author: Kien Tran
 */
--- src/surge/Panels/scripts/ip_check.js
+++ src/surge/Panels/scripts/ip_check.js
@@ -1,6 +1,6 @@
 /*!
  Made with ❤️ in Hanoi
- Build fullhash:7dd779982ddfe83cb09b, chunkhash:b72269e1917222b4b509 (2024-05-18 19:11:02)
+ Build fullhash:2af83bd1581f295b28b6, chunkhash:b72269e1917222b4b509 (2024-05-18 19:15:03)
 
  Author: Kien Tran
 */
