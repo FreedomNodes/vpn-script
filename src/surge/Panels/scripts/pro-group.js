49425981c9b5e578dfeae6538d845550501b0560
kien8995 <null>
2024-05-19 11:05:37 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/Panels/scripts/pro-group.js
+++ src/surge/Panels/scripts/pro-group.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:978b3a58eee77459663a, chunkhash:e7c84e442286a9ec5364 (2024-05-19 17:51:38)
+ Build fullhash:978b3a58eee77459663a, chunkhash:e7c84e442286a9ec5364 (2024-05-19 18:05:28)
 
  Author: Kien Tran
 */
--- src/surge/Testflight/scripts/auto_join_tf.js
+++ src/surge/Testflight/scripts/auto_join_tf.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:978b3a58eee77459663a, chunkhash:855607e113957bfcf92e (2024-05-19 17:51:38)
+ Build fullhash:978b3a58eee77459663a, chunkhash:855607e113957bfcf92e (2024-05-19 18:05:28)
 
  Author: Kien Tran
 */
