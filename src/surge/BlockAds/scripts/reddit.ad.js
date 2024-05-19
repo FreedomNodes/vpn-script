619844e51d2819969bbd1badaff6cadda1345ebf
kien8995 <null>
2024-05-19 19:44:03 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/BlockAds/scripts/reddit.ad.js
+++ src/surge/BlockAds/scripts/reddit.ad.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:98d7d1984e67eecaaa28 (2024-05-20 02:35:47)
+ Build fullhash:21d96bbaf966acf05890, chunkhash:98d7d1984e67eecaaa28 (2024-05-20 02:43:52)
 
  Author: Kien Tran
 */
