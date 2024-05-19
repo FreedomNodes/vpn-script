072dadca74d562f9025df202538cdffe0b28c41e
kien8995 <null>
2024-05-19 18:44:28 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/AdGuard/scripts/adguard-premium.js
+++ src/surge/AdGuard/scripts/adguard-premium.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:9bffc8c6e6823be1195a (2024-05-20 01:20:49)
+ Build fullhash:21d96bbaf966acf05890, chunkhash:9bffc8c6e6823be1195a (2024-05-20 01:44:18)
 
  Author: Kien Tran
 */
