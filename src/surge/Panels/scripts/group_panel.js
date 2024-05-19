73524f76250e29c1bfc9d98ac6a3bcf8d513dff0
kien8995 <null>
2024-05-19 06:20:55 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/Panels/scripts/group_panel.js
+++ src/surge/Panels/scripts/group_panel.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:978b3a58eee77459663a, chunkhash:f8efafea4f1c086181d6 (2024-05-19 13:08:40)
+ Build fullhash:978b3a58eee77459663a, chunkhash:f8efafea4f1c086181d6 (2024-05-19 13:20:45)
 
  Author: Kien Tran
 */
--- src/surge/Youtube/scripts/youtube.request.beta.js
+++ src/surge/Youtube/scripts/youtube.request.beta.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:978b3a58eee77459663a, chunkhash:4bd8373e5586a4c4698a (2024-05-19 13:08:40)
+ Build fullhash:978b3a58eee77459663a, chunkhash:4bd8373e5586a4c4698a (2024-05-19 13:20:45)
 
  Author: Kien Tran
 */
