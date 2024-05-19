f145a0ce14e95df4884c713bf4fef00f5f551723
kien8995 <null>
2024-05-19 19:54:59 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/Panels/scripts/group_panel.js
+++ src/surge/Panels/scripts/group_panel.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:f8efafea4f1c086181d6 (2024-05-20 02:48:28)
+ Build fullhash:21d96bbaf966acf05890, chunkhash:f8efafea4f1c086181d6 (2024-05-20 02:54:49)
 
  Author: Kien Tran
 */
--- src/surge/SubStore/scripts/substore-backup.js
+++ src/surge/SubStore/scripts/substore-backup.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:543cef4eaa42a2942597 (2024-05-20 02:48:28)
+ Build fullhash:21d96bbaf966acf05890, chunkhash:543cef4eaa42a2942597 (2024-05-20 02:54:49)
 
  Author: Kien Tran
 */
