e8fb2b8fd765b609855a4add380d0ff5c5f39d6d
kien8995 <null>
2024-05-20 06:59:39 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/Panels/scripts/net_info.js
+++ src/surge/Panels/scripts/net_info.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:25beb9db7793d103aa7f (2024-05-20 13:55:54)
+ Build fullhash:21d96bbaf966acf05890, chunkhash:25beb9db7793d103aa7f (2024-05-20 13:59:27)
 
  Author: Kien Tran
 */
