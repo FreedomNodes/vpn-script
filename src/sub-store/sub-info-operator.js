f7f460ef9406c7d2d86d3f2d91d2a8be89cfe08e
kien8995 <null>
2024-05-20 07:15:59 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/sub-info-operator.js
+++ src/sub-store/sub-info-operator.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:03e7a5e0ba5c8898990f, chunkhash:123848d4a8ab0855d0b8 (2024-05-20 03:20:51)
+ Build fullhash:03e7a5e0ba5c8898990f, chunkhash:123848d4a8ab0855d0b8 (2024-05-20 14:15:47)
 
  Author: Kien Tran
 */
