e54a208d3e84f0f4480aa4abb14b2b578f8c0ae4
kien8995 <null>
2024-05-20 21:31:02 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/SoundCloud/scripts/SoundCloudGo.js
+++ src/surge/SoundCloud/scripts/SoundCloudGo.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:c1f8d72aa5e10cfe972c (2024-05-21 04:25:15)
+ Build fullhash:21d96bbaf966acf05890, chunkhash:c1f8d72aa5e10cfe972c (2024-05-21 04:30:49)
 
  Author: Kien Tran
 */
