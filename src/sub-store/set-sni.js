caf1a6a3e19edd0cbe9a7e68b1afd941f10b03dd
kien8995 <null>
2024-05-20 06:59:39 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/set-sni.js
+++ src/sub-store/set-sni.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:f5ecfe6243ab457b6d4c (2024-05-20 13:55:54)
+ Build fullhash:21d96bbaf966acf05890, chunkhash:f5ecfe6243ab457b6d4c (2024-05-20 13:59:27)
 
  Author: Kien Tran
 */
