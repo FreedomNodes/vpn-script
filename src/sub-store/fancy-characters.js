6eb9ec2d9294e2b068b454c6a51321fc29069ad7
kien8995 <null>
2024-05-20 07:03:11 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/fancy-characters.js
+++ src/sub-store/fancy-characters.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:d8a35adf09abf991342d (2024-05-20 13:59:27)
+ Build fullhash:21d96bbaf966acf05890, chunkhash:d8a35adf09abf991342d (2024-05-20 14:02:58)
 
  Author: Kien Tran
 */
