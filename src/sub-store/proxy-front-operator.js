00c7eea39acae84697b4aef5bd00404a045989fc
kien8995 <null>
2024-05-20 22:12:55 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/proxy-front-operator.js
+++ src/sub-store/proxy-front-operator.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:5a17b5a086acc47da24e (2024-05-21 05:09:11)
+ Build fullhash:21d96bbaf966acf05890, chunkhash:5a17b5a086acc47da24e (2024-05-21 05:12:44)
 
  Author: Kien Tran
 */
