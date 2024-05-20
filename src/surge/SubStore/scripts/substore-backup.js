1f6d03c567077b15776fe57bbd2fd4d830697ce4
kien8995 <null>
2024-05-20 04:21:27 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/SubStore/scripts/substore-backup.js
+++ src/surge/SubStore/scripts/substore-backup.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:543cef4eaa42a2942597 (2024-05-20 03:08:58)
+ Build fullhash:21d96bbaf966acf05890, chunkhash:543cef4eaa42a2942597 (2024-05-20 11:21:14)
 
  Author: Kien Tran
 */
