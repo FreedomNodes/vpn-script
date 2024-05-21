8dbeeb05bbae29ed1b1d10fd8334ecda7505be85
kien8995 <null>
2024-05-21 12:50:00 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/Testflight/scripts/testflight.js
+++ src/surge/Testflight/scripts/testflight.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:03e7a5e0ba5c8898990f, chunkhash:41a0d3165d160b523103 (2024-05-20 14:15:47)
+ Build fullhash:4a3e90c2dfe6782e8caf, chunkhash:41a0d3165d160b523103 (2024-05-21 19:49:48)
 
  Author: Kien Tran
 */
