5f30d3a71e6f90b94bcf839b7c382dadc847742f
kien8995 <null>
2024-05-21 04:19:49 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/TrueCaller/scripts/truecaller.js
+++ src/surge/TrueCaller/scripts/truecaller.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:466ed6a4073c35b85643 (2024-05-21 10:35:55)
+ Build fullhash:feba361bf42b62d06d90, chunkhash:466ed6a4073c35b85643 (2024-05-21 11:19:37)
 
  Author: Kien Tran
 */
--- src/surge/Youtube/scripts/youtube.response.beta.js
+++ src/surge/Youtube/scripts/youtube.response.beta.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:21d96bbaf966acf05890, chunkhash:a1695cfb6edd3a876c24 (2024-05-21 10:35:55)
+ Build fullhash:feba361bf42b62d06d90, chunkhash:a1695cfb6edd3a876c24 (2024-05-21 11:19:37)
 
  Author: Kien Tran
 */
