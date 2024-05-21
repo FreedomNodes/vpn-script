a0b104f6c6a2dfd8ee779384aa545db55bc0358a
kien8995 <null>
2024-05-21 05:14:19 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/AppStore/scripts/revenuecat.js
+++ src/surge/AppStore/scripts/revenuecat.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:b4f7c34e61050c9585f0, chunkhash:26d09a469707f52313da (2024-05-21 12:05:13)
+ Build fullhash:b4f7c34e61050c9585f0, chunkhash:26d09a469707f52313da (2024-05-21 12:14:07)
 
  Author: Kien Tran
 */
