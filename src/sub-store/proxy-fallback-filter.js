7aef60354a872898eb4ee70c3aa0baa96e794a86
kien8995 <null>
2024-05-21 12:19:09 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/proxy-fallback-filter.js
+++ src/sub-store/proxy-fallback-filter.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:b4f7c34e61050c9585f0, chunkhash:30625681786dcf9319dc (2024-05-21 19:18:38)
+ Build fullhash:b4f7c34e61050c9585f0, chunkhash:30625681786dcf9319dc (2024-05-21 19:18:56)
 
  Author: Kien Tran
 */
