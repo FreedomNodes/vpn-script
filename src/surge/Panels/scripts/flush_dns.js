73247f757c56bc278b75db87f9d49c2da1fc5d51
kien8995 <null>
2024-05-22 14:41:20 +0000

    ✅ synced local 'src/surge/' with remote 'dist/surge/'

--- src/surge/Panels/scripts/flush_dns.js
+++ src/surge/Panels/scripts/flush_dns.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:4a3e90c2dfe6782e8caf, chunkhash:68358b59f138ac12ac3b (2024-05-22 21:03:54)
+ Build fullhash:4a3e90c2dfe6782e8caf, chunkhash:68358b59f138ac12ac3b (2024-05-22 21:41:01)
 
  Author: Kien Tran
 */
