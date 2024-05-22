1a1465c347ba1f59f5c1a21e690042b409e5837e
kien8995 <null>
2024-05-22 11:06:52 +0000

    ✅ synced local 'src/sub-store/' with remote 'dist/sub-store/'

--- src/sub-store/fancy-characters.js
+++ src/sub-store/fancy-characters.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:b4f7c34e61050c9585f0, chunkhash:d8a35adf09abf991342d (2024-05-22 16:06:26)
+ Build fullhash:b4f7c34e61050c9585f0, chunkhash:d8a35adf09abf991342d (2024-05-22 18:06:34)
 
  Author: Kien Tran
 */
--- src/sub-store/sub-info-operator.js
+++ src/sub-store/sub-info-operator.js
@@ -1,6 +1,6 @@
 /*!
  substore-tools - version: 1.8.1
- Build fullhash:b4f7c34e61050c9585f0, chunkhash:123848d4a8ab0855d0b8 (2024-05-22 16:06:26)
+ Build fullhash:b4f7c34e61050c9585f0, chunkhash:123848d4a8ab0855d0b8 (2024-05-22 18:06:34)
 
  Author: Kien Tran
 */
